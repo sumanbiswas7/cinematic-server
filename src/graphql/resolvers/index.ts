291ec22e4792bda9ffb63eacc65fe544ea3f9f28
suman <ronaldosuman97@gmail.com>
2023-01-28 15:09:46 +0530

    refactor: notificationMutations seperated

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,9 +1,10 @@
 import * as moviesQueries from "./queries/movieQueries";
 import * as userMutations from "./mutations/usersMutation";
+import * as notificationMutations from "./mutations/notificationMutations";
 import * as userQueries from "./queries/userQueries";
 
 export const Query = { ...moviesQueries, ...userQueries }
-export const Mutation = { ...userMutations }
+export const Mutation = { ...userMutations, ...notificationMutations }
 
 export const resolvers = { Query, Mutation }
 
--- src/graphql/resolvers/mutations/usersMutation.ts
+++ src/graphql/resolvers/mutations/usersMutation.ts
@@ -23,59 +23,6 @@ export async function create_user(_: any, args: { user: CreateUser }) {
     }
 }
 
-/**
- * Sends friend request to a user
- * @param args.request request object  
- */
-export async function send_request(_: any, args: { request: Notification }) {
-    const data = args.request
-    const createdAt = moment().format('MMMM Do YYYY, h:mm:ss a');
-    const newdata = { ...data, createdAt, suggestion: false, request: true }
-
-    try {
-        await prisma.notifications.create({ data: newdata })
-        return "request sent successfuly"
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-/**
- * Sends movie suggestion to a user
- * @param args.request request object  
- */
-export async function send_suggestion(_: any, args: { suggestion: Notification }) {
-    const data = args.suggestion
-    const createdAt = moment().format('MMMM Do YYYY, h:mm:ss a');
-    const newdata = { ...data, createdAt, suggestion: true, request: false }
-
-    try {
-        await prisma.notifications.create({ data: newdata })
-        return "request sent successfuly"
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
-/**
- * Deletes all notification of an user
- * @param args.userId user's id 
- */
-export async function clear_notifications(_: any, args: { userId: number }) {
-    const userId = args.userId
-
-    try {
-        await prisma.notifications.deleteMany({ where: { userId } })
-        return "Notifications cleared successfuly"
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
 
 interface CreateUser {
     name: string
@@ -83,11 +30,4 @@ interface CreateUser {
     password: string
     country?: string
     createdAt: string
-}
-
-interface Notification {
-    request: boolean
-    suggestion: boolean
-    from: string
-    userId: number
 }
